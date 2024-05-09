3c2e947be9346fdd576d577c1f60ee421e4dcf66
ikramul-islam <ikramul.mahin.islam@gmail.com>
2024-05-09 09:13:54 +0600

    newd

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthProvider from './context/AuthProvider/AuthProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
+import { ThemeProvider } from "@material-tailwind/react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient()
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
+        <ThemeProvider>
         <App />
+        </ThemeProvider>
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
--- src/pages/Home/Homes/Homes.js
+++ src/pages/Home/Homes/Homes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Cover from '../Cover/Cover';
 import HomeContent from '../HomeContent/HomeContent';
+import Video from '../Video/Video';
+import { MasonryGridGallery } from '../Galary/Galary';
 
 const Homes = () => {
     return (
         <div>
             <Banner></Banner>
             <Cover></Cover>
+            <MasonryGridGallery></MasonryGridGallery>
+            <Video></Video>
             <HomeContent></HomeContent>
         </div>
     );
